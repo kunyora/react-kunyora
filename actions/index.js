03406fc34cf88d35277dcc0afc98cfabca7d4e98
gbenga504 <daveanifowoshe@ymail.com>
2018-02-03 18:47:49 +0100

    Add actions, types and subscriber to the project

--- actions/index.js
+++ actions/index.js
@@ -4,4 +4,14 @@ export const setQuery = (operation, refinedState) => ({
   type: types.SET_QUERY_DATA,
   operation,
   refinedState
-})
+});
+
+export const sendRequestHeaderCommand = () => ({
+  type: types.COMPOSER_SEND_REQUEST_HEADER_CMD,
+  _reqObj: { options: {} }
+});
+
+export const sendResponseToMiddleware = response => ({
+  type: types.COMPOSER_SEND_RESPONSE_TO_MIDDLEWARE,
+  response
+});
