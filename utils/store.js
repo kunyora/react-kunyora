dfdb1d80fa96b7cb8e48294cfcacba5c05fde0b6
gbenga504 <daveanifowoshe@ymail.com>
2018-02-06 18:11:15 +0100

    Add store to the application

--- utils/store.js
+++ utils/store.js
@@ -1,47 +1,32 @@
-/**
- * @author Anifowoshe Gbenga David
- * This simple module models an Inmemory store for all Javascript based applications
- * using a reactive pattern of programming where an action is dispatched and a callback
- * is fired based on that action that has been dispatched
- * Series of callback could be added or listened to when a particular action is dispatched
- */
 export default (Store = {
-  actions: {},
-
   state: {},
 
-  /**
-   * @function gets the state of th store on the application
-   */
-  getState() {
-    return this.state;
+  callbacks: [],
+
+  listen: function(callback) {
+    this.callbacks.push(callback);
+    return this.nullify;
   },
 
-  /**
-   * @param {string} action
-   * @param {function} callback
-   * @function listens and adds callbacks to a particular action so that they can be called when their registered action is called
-   */
-  on(action, callback) {
-    this.actions[action] = this.actions[action] || [];
-    this.actions[action].push(callback);
+  nullify: function() {
+    this.listen = null;
   },
 
-  /**
-   * @param {string} action
-   * @param {any} args
-   * @function is called to dispatch or fire a specific action and then call the callbacks registered on the action at a later time
-   */
-  dispatch(action, ...args) {
-    if (!(action in this.actions)) return false;
-
-    const previousActionState = this.state.action || {};
-
+  dispatch: function(action, args) {
     this.state = {
       ...this.state,
-      action: { ...previousActionState, ...args }
+      action: args
     };
+    this.runCallbacks();
+  },
 
-    this.actions[action].forEach(cb => cb(...args));
+  runCallbacks: function() {
+    this.callbacks.forEach(callback => {
+      callback();
+    });
+  },
+
+  getState: function() {
+    return this.state;
   }
 });
