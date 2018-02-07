c68f5e4e8e4f8cdf90fa91e50e4ac5183dcf1419
gbenga504 <daveanifowoshe@ymail.com>
2018-02-07 20:36:15 +0100

    Add subscribe query helper methods to subscription

--- utils/subscriber.js
+++ utils/subscriber.js
@@ -1,40 +1,57 @@
 import * as actions from "../actions";
 import invariant from "invariant";
 
-function Subscriber(store, context) {
+function Subscriber(store, client) {
   this.store = store;
   this.client = client;
-  this.requestId = this.store.getState().requestHeader.id;
 }
 
-Subscriber.prototype.sendQuery = function(operation, downloadProgressCallback) {
-  let _promise = new Promise((resolve, reject) => {
-    let unsubscribe = this.store.subscribe(() => {
-      let { getState } = this.store,
-        { requestHeader: { options, id } } = getState();
-      if (id !== this.requestId) {
-        this.requestId = id;
-        unsubscribe();
-        //send query to axios backend here
-        let _downloadProgressCallback = downloadProgressCallback || undefined;
-        this.client[operation]({
-          onDownloadProgress: _downloadProgressCallback
-        })
-          .then(response => response)
-          .catch(error => reject(error));
-      }
-    });
-  });
-
+Subscriber.prototype.subscribeToQuery = function(
+  operation,
+  downloadProgressCallback
+) {
+  let _promise = null;
   if (this.client.isUseBeforeCallbackSupplied) {
-    let { defaults: { headers: { common } } } = this.client;
-    this.store.dispatch(actions.sendRequestHeaderCommand(common));
-    return _promise;
+    let { defaults: { headers } } = this.client,
+      requestHeaders = this.client.isUseBeforeCallbackSupplied(headers);
+
+    _promise = this.sendQuery(
+      requestHeaders,
+      downloadProgressCallback,
+      operation
+    );
+  } else {
+    _promise = this.sendQuery(null, downloadProgressCallback, operation);
   }
-  // else send request to get information from axios backend with the default common header
+  return _promise;
 };
 
-Subscriber.prototype.sendMutation = function(
-  operation,
-  uploadProgressMutation
-) {};
+Subscriber.prototype.sendQuery = function(
+  headers,
+  downloadProgressCallback,
+  operation
+) {
+  let _promise = new Promise((resolve, reject) => {
+    let _downloadProgressCallback = downloadProgressCallback || undefined;
+    if (headers) this.client.defaults.headers = headers;
+    this.client[operation]({
+      onDownloadProgress: _downloadProgressCallback
+    })
+      .then(response => {
+        this.sendResponseToCallback(response);
+        return response;
+      })
+      .catch(error => {
+        this.sendResponseToCallback(error);
+        reject(error);
+      });
+  });
+  return _promise;
+};
+
+Subscriber.prototype.sendResponseToCallback = function(response) {
+  let { isUseAfterCallbackSupplied } = this.client;
+  if (isUseAfterCallbackSupplied) {
+    isUseAfterCallbackSupplied(response);
+  }
+};
