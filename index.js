401970fc1bccb744a461296f83a8e71ed7386e59
gbenga504 <daveanifowoshe@ymail.com>
2018-02-10 19:27:12 +0100

    import Mutation in the index.js file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import ComposerProvider from "./components/ComposerProvider";
 import Queries from "./components/Queries";
+import Mutation from "./components/Mutation";
 import Store from "./utils/store";
 
-export { ComposerProvider, Queries, store };
+export { ComposerProvider, Queries, Mutation, store };
