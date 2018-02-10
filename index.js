c3365e3a5d5128b70cd1d7b281c42b3efd739e1d
gbenga504 <daveanifowoshe@ymail.com>
2018-02-10 19:33:07 +0100

    Make initial build

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import ComposerProvider from "./components/ComposerProvider";
-import Queries from "./components/Queries";
+import Queries from "./components/Query";
 import Mutation from "./components/Mutation";
 import Store from "./utils/store";
 
--- utils/subscriber.js
+++ utils/subscriber.js
@@ -5,7 +5,7 @@ function Subscriber(store, client) {
   this.client = client;
 }
 
-Subscriber.prototype.subscribeToQuery = function (
+Subscriber.prototype.subscribeToQuery = function(
   operation,
   downloadProgressCallback
 ) {
@@ -25,38 +25,50 @@ Subscriber.prototype.subscribeToQuery = function (
   return _promise;
 };
 
-Subscriber.prototype.subscribeToMutation = function (operation, uploadProgressCallback) {
+Subscriber.prototype.subscribeToMutation = function(
+  operation,
+  uploadProgressCallback
+) {
   let _promise = null;
   if (this.client.isUseBeforeCallbackSupplied) {
     let { defaults: { headers } } = this.client,
       requestHeaders = this.client.useAfterCallback(headers);
 
-    _promise = this.sendMutation(requestHeaders, uploadProgressCallback, operation);
-  }
-  else {
-    _promise = this.sendMutation(null, uploadProgressCallback, operation)
+    _promise = this.sendMutation(
+      requestHeaders,
+      uploadProgressCallback,
+      operation
+    );
+  } else {
+    _promise = this.sendMutation(null, uploadProgressCallback, operation);
   }
   return _promise;
-}
+};
 
-Subscriber.prototype.sendMutation = function (headers, uploadProgressCallback, operation) {
-  let _promise = new Promise((resolve, reject)) => {
+Subscriber.prototype.sendMutation = function(
+  headers,
+  uploadProgressCallback,
+  operation
+) {
+  let _promise = new Promise((resolve, reject) => {
     let _uploadProgressCallback = uploadProgressCallback || undefined;
     if (headers) this.client.defaults.headers = headers;
     this.client[operation]({
       onUploadProgress: _uploadProgressCallback
-    }).then(response => {
-      this.sendResponseToCallback(response);
-      return response;
-    }).catch(error => {
-      this.sendResponseToCallback(error);
-      reject(error);
     })
-  }
+      .then(response => {
+        this.sendResponseToCallback(response);
+        return response;
+      })
+      .catch(error => {
+        this.sendResponseToCallback(error);
+        reject(error);
+      });
+  });
   return _promise;
-}
+};
 
-Subscriber.prototype.sendQuery = function (
+Subscriber.prototype.sendQuery = function(
   headers,
   downloadProgressCallback,
   operation
@@ -79,7 +91,7 @@ Subscriber.prototype.sendQuery = function (
   return _promise;
 };
 
-Subscriber.prototype.sendResponseToCallback = function (response) {
+Subscriber.prototype.sendResponseToCallback = function(response) {
   let { isUseAfterCallbackSupplied, useAfterCallback } = this.client;
   if (isUseAfterCallbackSupplied) {
     useAfterCallback(response);
