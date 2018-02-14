d4756f8c63359bd19f7b80e2c2e675ca98298fde
gbenga504 <daveanifowoshe@ymail.com>
2018-02-14 16:42:58 +0100

    Resolve problems from export of Connect component in Query component

--- components/Query.js
+++ components/Query.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import invariant from "invariant";
 
-import { Connect } from "./Connect";
+import Connect from "./Connect";
 import * as types from "../types";
 import Subscriber from "../utils/subscriber";
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import ComposerProvider from "./components/ComposerProvider";
-import Queries from "./components/Query";
+import Query from "./components/Query";
 import Mutation from "./components/Mutation";
 import store from "./utils/store";
 
-export { ComposerProvider, Queries, Mutation, store };
+export { ComposerProvider, Query, Mutation, store };
