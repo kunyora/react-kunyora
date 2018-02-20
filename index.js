654a5c613ca2bd476f1ec3d8eb5b2c49f72cedb6
gbenga504 <daveanifowoshe@ymail.com>
2018-02-20 06:03:14 +0100

    Add Connector to the application

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import ComposerProvider from "./components/ComposerProvider";
 import Query from "./components/Query";
 import Mutation from "./components/Mutation";
+import Connector from "./components/Connector";
 import Router from "./components/Router";
 import store from "./utils/store";
 
-export { ComposerProvider, Query, Mutation, Router, store };
+export { ComposerProvider, Query, Mutation, Connector, Router, store };
