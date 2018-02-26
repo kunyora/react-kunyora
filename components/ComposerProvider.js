e18522dcf13ba7c474ec249fd433100776e023de
gbenga504 <daveanifowoshe@ymail.com>
2018-02-26 16:30:06 +0100

    Add comment to the ComposerProvider component

--- components/ComposerProvider.js
+++ components/ComposerProvider.js
@@ -2,11 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import createReactContext from "create-react-context";
 
+/**
+ * Here we use the polyfill context API provided by the react community
+ * We also export the ComposerContext variable so it would be called from other components
+ */
 export const ComposerContext = createReactContext({
   client: {},
   store: {}
 });
 
+/**
+ * The ComposerProvider class is an higher order class that wraps the whole ReactJs application
+ * It is used to accept the composer client instance and store as props and exposes them using the new context API to other lower level components
+ */
 export default class ComposerProvider extends React.PureComponent {
   static propTypes = {
     client: PropTypes.any,
