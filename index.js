0360c9515cccb02607641c9abd768bb77b8ef2c4
gbenga504 <daveanifowoshe@ymail.com>
2018-03-02 20:14:28 +0100

    Remove store imports

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ import Query from "./components/Query";
 import Mutation from "./components/Mutation";
 import Connector from "./components/Connector";
 import Router from "./components/Router";
-import store from "./utils/store";
 
-export { ComposerProvider, Query, Mutation, Connector, Router, store };
+export { ComposerProvider, Query, Mutation, Connector, Router };
