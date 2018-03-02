7d543ba875249fa0fb365c9379aa2ad739eb0aaa
gbenga504 <daveanifowoshe@ymail.com>
2018-03-02 19:25:11 +0100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
 const webpack = require("webpack");
 const path = require("path");
+const fs = require("fs");
+
+let nodeModules = {};
+fs
+  .readdirSync("node_modules")
+  .filter(function(x) {
+    return [".bin"].indexOf(x) == -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = "commonjs " + mod;
+  });
 
 module.exports = {
   entry: "./index.js",
